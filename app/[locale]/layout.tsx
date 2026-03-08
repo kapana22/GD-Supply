820d48f4c38c4e67e03555783cab65392a6f5364
Luka Kapanadze (200478) <luka-kapanadze@ug.edu.ge>
2026-03-08 14:04:02 +0400

    ...

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import type { Metadata } from "next";
 import dynamic from "next/dynamic";
 import { NextIntlClientProvider, createTranslator } from "next-intl";
